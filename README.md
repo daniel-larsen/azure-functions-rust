c611b1f5457ea71bc7897c3c11419bfe3da16af6
Daniel Larsen <44644910+daniel-larsen@users.noreply.github.com>
2022-11-08 15:14:03 -0400

    Added example, updated Cargo.toml, and added to README.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# azure-functions-rust
+# Rust Handler for Azure Functions (Work in Progress)
+
+This repository contains an Azure Functions handler for Rust.
+
+## Installation
+
+1. Add azure-functions-rust to your Cargo.toml file. Make sure tokio and hyper are all included.
+
+```toml
+[dependencies]
+tokio = { version = "1", features = ["rt", "macros", "rt-multi-thread"] }
+hyper = { version = "0.14", features = ["full"] }
+azure_functions = { git = "https://github.com/daniel-larsen/azure-functions-rust", branch = "main" }
+
+```
+2. Initalize the handler from your main.rs file. 
+
+```rust
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let keyvault_url = std::env::args()
+        .nth(1)
+        .expect("Missing KEYVAULT_URL environment variable.");
+
+    let creds = DefaultAzureCredential::default();
+    let keyvault_client = CertificateClient::new(keyvault_url.as_str(), Arc::new(creds))?;
+
+    let environment = Environment {
+        certificate_client: keyvault_client,
+    };
+
+    azure_func_init(environment).await;
+    Ok(())
+}
+
+```
---
+++ example/src/main.rs
@@ -0,0 +1,93 @@
+use azure_functions::{http::HttpMethod, FunctionPayload, FunctionsResponse, HttpStatusCode};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use serde_json::Error;
+
+async fn process_request(
+    payload: FunctionPayload,
+    response: &mut FunctionsResponse,
+    env: Environment,
+) {
+    match payload {
+        FunctionPayload::HttpData(payload) => {
+            match (payload.data.req.method, payload.data.req.url.path()) {
+                (HttpMethod::Get, "test") => {
+                    response.outputs.res.body = "Success".to_string();
+                    response.outputs.res.status_code = HttpStatusCode::Ok;
+                }
+                _ => response.outputs.res.body = "path not found".to_string(),
+            }
+        }
+        FunctionPayload::EventHubData(payload) => {}
+
+        FunctionPayload::TimerData(payload) => {}
+        _ => response.outputs.res.body = "payload type not found".to_string(),
+    }
+}
+
+// This is our service handler. It receives a Request, routes on its
+// path, and returns a Future of a Response.
+async fn request_handler(
+    request: Request<Body>,
+    env: Environment,
+) -> Result<Response<Body>, hyper::Error> {
+    println!("{}", request.uri());
+
+    let bytes = hyper::body::to_bytes(request.into_body()).await.unwrap();
+    let vector: Vec<u8> = bytes.to_vec();
+    println!("{:?}", std::str::from_utf8(&vector).unwrap());
+    let deserialize_request: Result<FunctionPayload, Error> = serde_json::from_slice(&vector);
+
+    let mut response: FunctionsResponse = Default::default();
+
+    if deserialize_request.is_err() {
+        response.outputs.res.body = deserialize_request.err().unwrap().to_string();
+    } else {
+        process_request(deserialize_request.unwrap(), &mut response, env).await;
+    }
+
+    let response_string: String = serde_json::to_string(&response).unwrap();
+    println!("{}", response_string);
+    let hyper_response = Response::builder()
+        .status(200)
+        .header("content-type", "application/json")
+        .body(Body::from(response_string))
+        .unwrap();
+
+    Ok(hyper_response)
+}
+
+async fn azure_func_init(env: Environment) {
+    let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
+    let port: u16 = match std::env::var(port_key) {
+        Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
+        Err(_) => 3000,
+    };
+
+    let addr = ([127, 0, 0, 1], port).into();
+    let service = make_service_fn(move |_| {
+        let env = env.clone();
+        async move { Ok::<_, hyper::Error>(service_fn(move |req| request_handler(req, env.clone()))) }
+    });
+    let server = Server::bind(&addr).serve(service);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Environment {}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let keyvault_url = std::env::args()
+        .nth(1)
+        .expect("Missing KEYVAULT_URL environment variable.");
+
+    let environment = Environment {};
+
+    azure_func_init(environment).await;
+    Ok(())
+}
