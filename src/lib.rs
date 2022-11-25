7b2691e82d13d171447b0103e9a78a7a4c60107f
Daniel Larsen <44644910+daniel-larsen@users.noreply.github.com>
2022-11-25 11:59:37 -0400

    Removed several unwrap functions. Try to return errors so they can be logged in Application Insights.

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,19 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::fmt::Error;
+use std::fmt::Display;
 use std::future::Future;
 
 use self::event_hub::EventHubPayload;
 use self::http::HttpPayload;
 use self::timer::TimerPayload;
 
-pub async fn azure_func_init<F, E, S>(handler: F, env: E)
+pub async fn azure_func_init<F, S, R, E>(handler: F, env: S)
 where
-    F: Fn(FunctionPayload, E) -> S + std::marker::Send + 'static + Copy + std::marker::Sync,
-    E: Clone + std::marker::Send + 'static,
-    S: Future<Output = Result<FunctionsResponse, Error>> + std::marker::Send + 'static,
+    F: Fn(FunctionPayload, S) -> R + std::marker::Send + 'static + Copy + std::marker::Sync,
+    S: Clone + std::marker::Send + 'static,
+    R: Future<Output = Result<FunctionsResponse, E>> + std::marker::Send + 'static,
+    E: Display + 'static,
 {
     let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
     let port: u16 = match std::env::var(port_key) {
@@ -42,39 +43,61 @@ where
     }
 }
 
+pub fn log_error(error: String) -> Response<Body> {
+    return Response::builder()
+        .status(200)
+        .header("content-type", "application/json")
+        .body(Body::from(format!(
+            r#"{{"Outputs":{{"res":{{"body":"An error occurred while processing the request, check the log for a detailed error message.","statusCode":"400","headers":{{}}}}}},"Logs":["{}"]}}"#,
+            error
+        )))
+        .unwrap();
+}
+
 // This is our service handler. It receives a Request, routes on its
 // path, and returns a Future of a Response.
-pub async fn request_handler<F, E, S>(
+pub async fn request_handler<F, S, R, E>(
     request: Request<Body>,
     handler: F,
-    env: E,
+    env: S,
 ) -> Result<Response<Body>, hyper::Error>
 where
-    F: Fn(FunctionPayload, E) -> S + std::marker::Send + 'static + Copy,
-    E: Clone + std::marker::Send + 'static,
-    S: Future<Output = Result<FunctionsResponse, Error>> + std::marker::Send + 'static,
+    F: Fn(FunctionPayload, S) -> R + std::marker::Send + 'static + Copy,
+    S: Clone + std::marker::Send + 'static,
+    R: Future<Output = Result<FunctionsResponse, E>> + std::marker::Send + 'static,
+    E: Display + 'static,
 {
-    let bytes = hyper::body::to_bytes(request.into_body()).await.unwrap();
+    let bytes = match hyper::body::to_bytes(request.into_body()).await {
+        Ok(bytes) => bytes,
+        Err(error) => return Ok(log_error(error.to_string())),
+    };
+
     let vector: Vec<u8> = bytes.to_vec();
     // println!("{:?}", std::str::from_utf8(&vector).unwrap());
-    let deserialize_request: Result<FunctionPayload, serde_json::Error> =
-        serde_json::from_slice(&vector);
+    let deserialize_request: FunctionPayload = match serde_json::from_slice(&vector) {
+        Ok(deserialize_request) => deserialize_request,
+        Err(error) => return Ok(log_error(error.to_string())),
+    };
 
-    let mut response: FunctionsResponse = Default::default();
+    let response = match handler(deserialize_request, env).await {
+        Ok(response) => response,
+        Err(error) => return Ok(log_error(error.to_string())),
+    };
 
-    if deserialize_request.is_err() {
-        response.outputs.res.body = deserialize_request.err().unwrap().to_string();
-    } else {
-        response = handler(deserialize_request.unwrap(), env).await.unwrap();
-    }
+    let response_string: String = match serde_json::to_string(&response) {
+        Ok(response_string) => response_string,
+        Err(error) => return Ok(log_error(error.to_string())),
+    };
 
-    let response_string: String = serde_json::to_string(&response).unwrap();
-    // println!("{}", response_string);
-    let hyper_response = Response::builder()
+    //println!("{}", response_string);
+    let hyper_response = match Response::builder()
         .status(200)
         .header("content-type", "application/json")
         .body(Body::from(response_string))
-        .unwrap();
+    {
+        Ok(hyper_response) => hyper_response,
+        Err(error) => return Ok(log_error(error.to_string())),
+    };
 
     Ok(hyper_response)
 }
