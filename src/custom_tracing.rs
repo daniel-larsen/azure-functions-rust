dacd6ae19427996ce9ec0d2f4870513f452776a5
Daniel Larsen <44644910+daniel-larsen@users.noreply.github.com>
2023-02-25 23:48:08 -0400

    Improved formatting of trace messages

--- src/custom_tracing.rs
+++ src/custom_tracing.rs
@@ -30,8 +30,58 @@ where
         event: &tracing::Event<'_>,
         _ctx: tracing_subscriber::layer::Context<'_, S>,
     ) {
-        if *event.metadata().level() > self.level {
-            self.events.lock().unwrap().push(format!("{:#?}", event));
+        let mut visitor = CustomVisitor::new();
+        event.record(&mut visitor);
+
+        if *event.metadata().level() <= self.level {
+            self.events.lock().unwrap().push(format!(
+                "{} {:?} {}",
+                event.metadata().level(),
+                visitor.0,
+                event.metadata().name()
+            ));
         }
+    }
+}
+
+struct CustomVisitor(Vec<String>);
+
+impl CustomVisitor {
+    fn new() -> Self {
+        Self { 0: Vec::new() }
+    }
+}
+
+impl<'a> tracing::field::Visit for CustomVisitor {
+    fn record_f64(&mut self, field: &tracing::field::Field, value: f64) {
+        self.0.push(format!("{}: {} ", field.name(), value));
+    }
+
+    fn record_i64(&mut self, field: &tracing::field::Field, value: i64) {
+        self.0.push(format!("{}: {} ", field.name(), value));
+    }
+
+    fn record_u64(&mut self, field: &tracing::field::Field, value: u64) {
+        self.0.push(format!("{}: {} ", field.name(), value));
+    }
+
+    fn record_bool(&mut self, field: &tracing::field::Field, value: bool) {
+        self.0.push(format!("{}: {} ", field.name(), value));
+    }
+
+    fn record_str(&mut self, field: &tracing::field::Field, value: &str) {
+        self.0.push(format!("{}: {} ", field.name(), value));
+    }
+
+    fn record_error(
+        &mut self,
+        field: &tracing::field::Field,
+        value: &(dyn std::error::Error + 'static),
+    ) {
+        self.0.push(format!("{}: {} ", field.name(), value));
+    }
+
+    fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
+        self.0.push(format!("{}: {:?} ", field.name(), value));
     }
 }
